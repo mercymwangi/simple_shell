47eb8be177925d3b24cb55956317fadaa63bdcc7
mercymwangi <mercien.m@gmail.com>
2023-07-18 07:11:50 -0700

    betty file

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "shell.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int a = 10;
+	int b = 15;
+
+	int sum = (a + b);
+
+	printf("Result: %d\n", sum);
+
+	return (0);
+}
